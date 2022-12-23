a3f1a198e3526bbabb7f191ce701fd0316afd95c
awsk1994 <awsk1994@gmail.com>
2022-12-24 00:14:05 +0800

    add factory impl

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -6,16 +6,22 @@ import { DealClient } from "./DealClient.sol";
 
 contract Factory{
     DealClient[] public dealClients;
+    string[] public dealClientsDesc;
 
     event DealClientCreated(address addr, string description);
 
-    function createClient(string memory _description, string memory _example, uint64 _initialInvestmentTarget, uint64 _purchasePrice) external {
-        DealClient newDealClient = new DealClient(_description, _example, _initialInvestmentTarget, _purchasePrice);
+    function createClient(string memory _description, uint64 _initialInvestmentTarget, uint64 _purchasePrice) public {
+        DealClient newDealClient = new DealClient(_description, "", _initialInvestmentTarget, _purchasePrice);
         dealClients.push(newDealClient);
+        dealClientsDesc.push(_description);
         emit DealClientCreated(address(newDealClient), _description);
     }
 
     function getClients() public view returns (DealClient[] memory) {
         return dealClients;
+    }
+    
+    function getClientsDesc() public view returns (string[] memory) {
+        return dealClientsDesc;
     }
 }
