6cf59ee8dee37aeb139d477aab13989fd4f015d2
alexskwong <alexskwong@tencent.com>
2022-12-24 22:50:07 +0800

    update factory contract

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -8,13 +8,10 @@ contract Factory{
     DealClient[] public dealClients;
     string[] public dealClientsDesc;
 
-    event DealClientCreated(address addr, string description);
-
     function createClient(string memory _description, uint64 _initialInvestmentTarget, uint64 _purchasePrice) public {
         DealClient newDealClient = new DealClient(_description, "", _initialInvestmentTarget, _purchasePrice, msg.sender);
         dealClients.push(newDealClient);
         dealClientsDesc.push(_description);
-        emit DealClientCreated(address(newDealClient), _description);
     }
 
     function getClients() public view returns (DealClient[] memory) {
